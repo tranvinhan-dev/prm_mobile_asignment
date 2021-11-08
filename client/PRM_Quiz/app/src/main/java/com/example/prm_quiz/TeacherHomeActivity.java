909cd4698c699838fb965df14a967735ac97ee68
luuquangnhat <nhatluu9991@gmail.com>
2021-11-08 19:57:49 +0700

    banner for student, teacher 1st look

--- client/PRM_Quiz/app/src/main/java/com/example/prm_quiz/TeacherHomeActivity.java
+++ client/PRM_Quiz/app/src/main/java/com/example/prm_quiz/TeacherHomeActivity.java
@@ -1,31 +1,52 @@
 package com.example.prm_quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.prm_quiz.adapter.CustomListQuizForAdminAdapter;
+import com.example.prm_quiz.adapter.PhotoViewPage2Adapter;
 import com.example.prm_quiz.api.ApiClient;
+import com.example.prm_quiz.model.Photo;
 import com.example.prm_quiz.model.Quiz;
 import com.example.prm_quiz.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator3;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class TeacherHomeActivity extends AppCompatActivity {
     SharedPreferences prefs;
-    List<Quiz> quizList  ;
+    List<Quiz> quizList ;
+    //banner
+    private ViewPager2 mViewPager2;
+    private CircleIndicator3 mCircleIndicator3;
+    private List<Photo> mListPhoto;
+    //auto-run banner
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mViewPager2.getCurrentItem() == mListPhoto.size() - 1){
+                mViewPager2.setCurrentItem(0);
+            } else {
+                mViewPager2.setCurrentItem(mViewPager2.getCurrentItem() + 1);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +72,23 @@ public class TeacherHomeActivity extends AppCompatActivity {
                 removeToken();
                 Intent intent = new Intent(TeacherHomeActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+        //banner
+        mViewPager2 = findViewById(R.id.view_pager_2);
+        mCircleIndicator3 = findViewById(R.id.circle_indicator_3);
+        mListPhoto = getListPhoto();
+        PhotoViewPage2Adapter adapter = new PhotoViewPage2Adapter(mListPhoto);
+        mViewPager2.setAdapter(adapter);
+        //connect mCircleIndicator3 with mViewPager2
+        mCircleIndicator3.setViewPager(mViewPager2);
+        //auto-run banner
+        mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mHandler.removeCallbacks(mRunnable);
+                mHandler.postDelayed(mRunnable, 3000);
             }
         });
     }
@@ -102,5 +140,25 @@ public class TeacherHomeActivity extends AppCompatActivity {
                 Toast.makeText(TeacherHomeActivity.this, "Call Api Error" + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private List<Photo> getListPhoto(){
+        List<Photo> list = new ArrayList<>();
+        list.add(new Photo(R.drawable.p1));
+        list.add(new Photo(R.drawable.p2));
+        list.add(new Photo(R.drawable.p3));
+        return list;
+    }
+
+    @Override
+    protected void onPause() {//banner - When out the app (ex: Press 'home')
+        super.onPause();
+        mHandler.removeCallbacks(mRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(mRunnable, 3000);
     }
 }
